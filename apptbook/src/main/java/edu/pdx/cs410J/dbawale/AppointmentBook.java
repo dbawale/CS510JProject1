7addd9643e3c5d6a249fc8e0945b62a242b7048f
Deven Bawale <dbawale@pdx.edu>
2016-06-27 22:54:58 -0700

    Added AppointmentBook.java. No functionality implemented yet.

---
+++ apptbook/src/main/java/edu/pdx/cs410J/dbawale/AppointmentBook.java
@@ -0,0 +1,28 @@
+package edu.pdx.cs410J.dbawale;
+
+import edu.pdx.cs410J.AbstractAppointment;
+import edu.pdx.cs410J.AbstractAppointmentBook;
+
+import java.util.Collection;
+
+/**
+ * Created by Deven on 6/27/2016.
+ */
+public class AppointmentBook extends AbstractAppointmentBook {
+
+
+    @Override
+    public String getOwnerName() {
+        return null;
+    }
+
+    @Override
+    public Collection getAppointments() {
+        return null;
+    }
+
+    @Override
+    public void addAppointment(AbstractAppointment abstractAppointment) {
+
+    }
+}
