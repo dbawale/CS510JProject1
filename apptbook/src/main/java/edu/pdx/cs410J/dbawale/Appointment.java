99d42cb5e03741ddd20d12e323b7f5ed63814ad3
Deven Bawale <dbawale@pdx.edu>
2016-06-28 20:51:56 -0700

    Added fields to Appointment.java and added tests for testing the functionality of Appointment.java

--- apptbook/src/main/java/edu/pdx/cs410J/dbawale/Appointment.java
+++ apptbook/src/main/java/edu/pdx/cs410J/dbawale/Appointment.java
@@ -3,18 +3,28 @@ package edu.pdx.cs410J.dbawale;
 import edu.pdx.cs410J.AbstractAppointment;
 
 public class Appointment extends AbstractAppointment {
+  String description;
+  String beginTime;
+  String endTime;
+
+  public Appointment(String description, String beginTime, String endTime) {
+    this.description=description;
+    this.beginTime=beginTime;
+    this.endTime=endTime;
+  }
+
   @Override
   public String getBeginTimeString() {
-    throw new UnsupportedOperationException("This method is not implemented yet");
+    return this.beginTime;
   }
 
   @Override
   public String getEndTimeString() {
-    throw new UnsupportedOperationException("This method is not implemented yet");
+    return this.endTime;
   }
 
   @Override
   public String getDescription() {
-    return "This method is not implemented yet";
+    return this.description;
   }
 }
--- apptbook/src/test/java/edu/pdx/cs410J/dbawale/AppointmentTest.java
+++ apptbook/src/test/java/edu/pdx/cs410J/dbawale/AppointmentTest.java
@@ -4,28 +4,40 @@ import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Unit tests for the {@link Appointment} class.
  */
 public class AppointmentTest {
-
-  @Test(expected = UnsupportedOperationException.class)
-  public void getBeginTimeStringNeedsToBeImplemented() {
-    Appointment appointment = new Appointment();
-    appointment.getBeginTimeString();
+  @Test
+  public void getBeginTimeStringShouldReturnCorrectBeginTime()
+  {
+    Appointment appointment = new Appointment("hello","00","01");
+    assertEquals(appointment.getBeginTimeString(),"00");
   }
 
   @Test
-  public void initiallyAllAppointmentsHaveTheSameDescription() {
-    Appointment appointment = new Appointment();
-    assertThat(appointment.getDescription(), containsString("not implemented"));
+  public void newAppointmentConstructorTest()
+  {
+    Appointment appointment = new Appointment("hello","00","01");
+    assertEquals(appointment.beginTime,"00");
+    assertEquals(appointment.endTime,"01");
+    assertEquals(appointment.description,"hello");
   }
 
   @Test
-  public void forProject1ItIsOkayIfGetBeginTimeReturnsNull() {
-    Appointment appointment = new Appointment();
-    assertThat(appointment.getBeginTime(), is(nullValue()));
+  public void getEndTimeStringShouldReturnCorrectBeginTime()
+  {
+    Appointment appointment = new Appointment("hello","00","01");
+    assertEquals(appointment.getEndTimeString(),"01");
+  }
+
+  @Test
+  public void getAppointmentDescription()
+  {
+    Appointment appointment = new Appointment("hello","00","01");
+    assertEquals(appointment.getDescription(),"hello");
   }
 
 }
