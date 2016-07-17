9aac2a6ff0c5df9c502b760ca84f788a130751ad
Deven Bawale <dbawale@pdx.edu>
2016-07-17 14:47:29 -0700

    Completed PrettyPrinter implementation and added unit tests for PrettyPrinting

--- apptbook/src/main/java/edu/pdx/cs410J/dbawale/PrettyPrinter.java
+++ apptbook/src/main/java/edu/pdx/cs410J/dbawale/PrettyPrinter.java
@@ -3,7 +3,11 @@ package edu.pdx.cs410J.dbawale;
 import edu.pdx.cs410J.AbstractAppointmentBook;
 import edu.pdx.cs410J.AppointmentBookDumper;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Pretty printer class for Appointment Book project.
@@ -13,6 +17,10 @@ import java.io.IOException;
 public class PrettyPrinter implements AppointmentBookDumper {
     private String filename;
 
+    PrettyPrinter(String filename){
+        this.filename=filename;
+    }
+
     /**
      * Pretty prints the appointment book to the file specified.
      *
@@ -21,6 +29,41 @@ public class PrettyPrinter implements AppointmentBookDumper {
      */
     @Override
     public void dump(AbstractAppointmentBook abstractAppointmentBook) throws IOException {
-
+        FileOutputStream ostream = new FileOutputStream(filename);
+        DateFormat df = DateFormat.getDateTimeInstance(DateFormat.SHORT,DateFormat.SHORT);
+        ArrayList<Appointment> currentAppointments = (ArrayList)abstractAppointmentBook.getAppointments();
+        ostream.write("This appoinment book belongs to: ".getBytes());
+        ostream.write(abstractAppointmentBook.getOwnerName().getBytes());
+        ostream.write("\n".getBytes());
+        String str = "There are " + currentAppointments.size() + " appointments in this appointment book.\n\n";
+        ostream.write(str.getBytes());
+        str = "The appointments are:\n\n";
+        ostream.write(str.getBytes());
+        int i=1;
+        for(Appointment appt : currentAppointments)
+        {
+            str = i + ": " + appt.getDescription() + "\n";
+            ostream.write(str.getBytes());
+            str = "   Starts at: " + df.format(appt.beginTime) + "\t\t" + "Ends at: " + df.format(appt.endTime) + "\n";
+            ostream.write(str.getBytes());
+            long diff = appt.endTime.getTime() - appt.beginTime.getTime();
+            str = "   The duration of this appointment is " + TimeUnit.MILLISECONDS.toMinutes(diff) + " minutes. \n\n";
+            ostream.write(str.getBytes());
+            i++;
+        }
+//        ostream.write(abstractAppointmentBook.getOwnerName().getBytes());
+//        ostream.write("\n".getBytes());
+//        ArrayList<Appointment> currentappointments = new ArrayList<Appointment>();
+//        currentappointments = (ArrayList)abstractAppointmentBook.getAppointments();
+//        for (Appointment appointment : currentappointments) {
+//            ostream.write(appointment.description.getBytes());
+//            ostream.write("*#*".getBytes());
+//            //ostream.write(appointment.beginTime.toString().getBytes());
+//            ostream.write(df.format(appointment.beginTime).getBytes());
+//            ostream.write("*#*".getBytes());
+//            //ostream.write(appointment.endTime.toString().getBytes());
+//            ostream.write(df.format(appointment.endTime).getBytes());
+//            ostream.write("\n".getBytes());
+//        }
     }
 }
---
+++ apptbook/src/test/java/edu/pdx/cs410J/dbawale/PrettyPrinterTest.java
@@ -0,0 +1,49 @@
+package edu.pdx.cs410J.dbawale;
+
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import static java.lang.System.exit;
+import static org.junit.Assert.*;
+
+public class PrettyPrinterTest {
+    @Test
+    public void TestPrettyPrinterWithOneAppt()
+    {
+        AppointmentBook book = new AppointmentBook("deven");
+        Appointment appt =createAppointmentWithHello();
+        book.addAppointment(appt);
+        PrettyPrinter pretty = new PrettyPrinter("pretty.txt");
+        try {
+            pretty.dump(book);
+        } catch (IOException e) {
+            System.out.println("Error with file\n");
+            exit(1);
+        }
+    }
+
+    @Test
+    public void TestPrettyPrinterWithTwoAppts()
+    {
+        AppointmentBook book = new AppointmentBook("deven");
+        Appointment appt =createAppointmentWithHello();
+        Appointment appt1 = new Appointment("hello2",new Date("7/14/2016 11:24 am"),new Date("7/17/2016 11:00 am"));
+        book.addAppointment(appt);
+        book.addAppointment(appt1);
+        PrettyPrinter pretty = new PrettyPrinter("pretty.txt");
+        try {
+            pretty.dump(book);
+        } catch (IOException e) {
+            System.out.println("Error with file\n");
+            exit(1);
+        }
+    }
+
+    private Appointment createAppointmentWithHello() {
+        return new Appointment("hello",new Date("7/16/2016 11:24 am"), new Date("7/17/2016 11:24 am"));
+    }
+}
